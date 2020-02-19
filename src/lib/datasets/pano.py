6d0ec533d00e519d1fb7c85ad5fce2c735a91cb3
CHAEN <nuguziii@naver.com>
2020-02-19 10:57:29 +0900

    add pano files

---
+++ src/lib/datasets/pano.py
@@ -0,0 +1,185 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import cv2
+import glob
+
+class PANODataset:
+    def __init__(self, path):
+        super(PANODataset, self).__init__()
+        self.data_dir = path
+        self.w_pad = 100
+        self.h_pad = 0
+        self.ratio = 2
+
+    def getImgIds(self):
+        file_list = glob.glob(self.data_dir + '/*.jpg')
+        return file_list
+
+    def _loadImg(self, file):
+        return cv2.imread(file, cv2.IMREAD_COLOR)
+
+    def loadData(self, file):
+        original_img = self._loadImg(file)
+        img, anns = self._cropImg(original_img, self._loadAnn(file))
+        img, anns = self._resize(img, anns)
+        img = self._normalize(img)
+
+        #for ann in anns:
+        #    ann["bbox"] = self.getOriginalCoord(original_img, ann["bbox"])
+
+        #self.showImage(original_img, anns)
+        return img, anns
+
+    def getOriginalCoord(self, original, bbox):
+        width = np.size(original, 1)
+        height = np.size(original, 0)
+
+        original = original[self.h_pad:height - self.h_pad, self.w_pad:width - self.w_pad, :]
+
+        width = np.size(original, 1)
+        height = np.size(original, 0)
+
+        if height * self.ratio < width:
+            newH = height
+            newW = height * self.ratio
+        elif height * self.ratio > width:
+            newH = int(width / self.ratio)
+            newW = newH * self.ratio
+        else:
+            newH = height
+            newW = width
+
+        diffH = int((height - newH) / 2)
+        diffW = int((width - newW) / 2)
+
+        original = original[diffH:diffH + newH, diffW:diffW + newW, :]
+
+        width = np.size(original, 1)
+        height = np.size(original, 0)
+
+        fx = width / 512
+        fy = height / 256
+
+        bbox = (bbox * np.array([fx, fy, fx, fy])).astype(int) + np.array([diffW + self.w_pad, diffH + self.h_pad, diffW + self.w_pad, diffH + self.h_pad])
+
+        return bbox
+
+    def _normalize(self, img):
+        inp = (img.astype(np.float32) / 255.)
+
+        val = np.reshape(inp, (-1,3))
+        mean = np.mean(val, axis=0).reshape(1, 1, 3)
+        std = np.std(val, axis=0).reshape(1, 1, 3)
+
+        inp = (inp - mean) / std
+        return inp
+
+    def _resize(self, img, anns):
+        img_new = cv2.resize(img, (512, 256), interpolation=cv2.INTER_CUBIC)
+        fx = np.size(img, 1) / 512
+        fy = np.size(img, 0) / 256
+
+        for ann in anns:
+            ann["bbox"] = (ann["bbox"]/np.array([fx, fy, fx, fy])).astype(int)
+            ann["center"] = (ann["center"]/np.array([fx, fy])).astype(int)
+
+        return img_new, anns
+
+    def _cropImg(self, img, anns):
+        width = np.size(img, 1)
+        height = np.size(img, 0)
+
+        img = img[self.h_pad:height-self.h_pad, self.w_pad:width-self.w_pad, :]
+
+        width = np.size(img, 1)
+        height = np.size(img, 0)
+
+        if height*self.ratio<width:
+            newH = height
+            newW = height*self.ratio
+        elif height*self.ratio>width:
+            newH = int(width/self.ratio)
+            newW = newH*self.ratio
+        else:
+            newH = height
+            newW = width
+
+        diffH = int((height-newH)/2)
+        diffW = int((width-newW)/2)
+        img = img[diffH:diffH+newH,diffW:diffW+newW,:]
+
+        for ann in anns:
+            ann["bbox"] = ann["bbox"] - np.array([diffW + self.w_pad, diffH + self.h_pad, diffW + self.w_pad, diffH + self.h_pad])
+            ann["center"] = ann["center"] - np.array([diffW + self.w_pad, diffH + self.h_pad])
+
+        return img, anns
+
+    def _loadAnn(self, file):
+        '''
+        :param file: pano jpg image path
+        :return data: [ {"num":1, "bbox":[(0,0),(0,0),(0,0),(0,0)], "center":(0,0)} , {} , ... ]
+        '''
+        data = []
+
+        img = cv2.imread(file, cv2.IMREAD_COLOR)
+        centerX = int(np.size(img, 1) / 2)
+        centerY = int(np.size(img, 0) / 2)
+
+        f = open(file.replace('jpg', 'txt'), 'r')
+        txt = f.readlines()
+        f.close()
+        for t in txt:
+            dict = {}
+            t = t.replace('\n', '').split(', ')
+
+            x_coord = []
+            y_coord = []
+
+            for i in range(8):
+                x_coord.append(centerX + int(t[2 * i + 3]))
+                y_coord.append(centerY + int(t[2 * i + 4]))
+
+            coords = [min(x_coord), min(y_coord), max(x_coord), max(y_coord)]  # minX, minY, maxX, maxY
+
+            dict["bbox"] = np.array([coords[0], coords[1], coords[2], coords[3]])  # from top-left corner, clockwise
+            dict["num"] = t[0]
+            dict["center"] = np.array([centerX + int(t[27]), centerY + int(t[28])])
+            if t[1]=='True':
+                dict["category_id"] = 1
+            else:
+                dict["category_id"] = 2
+
+            data.append(dict)
+
+        return data
+
+    def showImage(self, img, anns):
+        for ann in anns:
+            bbox = ann["bbox"]
+            center = ann["center"]
+            num = ann["num"]
+            category_id = ann["category_id"]
+
+            cv2.line(img, (bbox[0],bbox[1]), (bbox[2],bbox[1]), (0, 255, 0), 1)
+            cv2.line(img, (bbox[2], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 1)
+            cv2.line(img, (bbox[2], bbox[3]), (bbox[0], bbox[3]), (0, 255, 0), 1)
+            cv2.line(img, (bbox[0], bbox[3]), (bbox[0], bbox[1]), (0, 255, 0), 1)
+
+            cv2.putText(img, num, (center[0], center[1]), cv2.FONT_ITALIC, 0.5, (0, 255, 255), 1)
+            if category_id == 1:
+                cv2.circle(img, (center[0], center[1]), 3, (0, 0, 255), -1)
+            else:
+                cv2.circle(img, (center[0], center[1]), 3, (255, 0, 0), -1)
+
+        cv2.imshow("result", img)
+        cv2.waitKey(0)
+
+if __name__ == "__main__":
+    pano = PANODataset('E:\\dataset\\0210-anonymous\\train')
+    file_lists = pano.getImgIds()
+    for file in file_lists:
+        pano.loadData(file)
+
---
+++ src/lib/detectors/pano.py
@@ -0,0 +1,196 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+try:
+    from external.nms import soft_nms
+except:
+    print('NMS not imported! If you need it,'
+          ' do \n cd $CenterNet_ROOT/src/lib/external \n make')
+from models.decode import ctdet_decode
+from models.utils import flip_tensor
+from utils.post_process import ctdet_post_process
+
+import cv2
+import numpy as np
+import time
+import torch
+
+from models.model import create_model, load_model
+from utils.image import get_affine_transform
+from utils.debugger import Debugger
+
+
+class CtdetDetector(object):
+    def __init__(self, opt):
+        if opt.gpus[0] >= 0:
+            opt.device = torch.device('cuda')
+        else:
+            opt.device = torch.device('cpu')
+
+        print('Creating model...')
+        self.model = create_model(opt.arch, opt.heads, opt.head_conv)
+        self.model = load_model(self.model, opt.load_model)
+        self.model = self.model.to(opt.device)
+        self.model.eval()
+
+        self.max_per_image = 32
+        self.num_classes = opt.num_classes
+        self.scales = opt.test_scales
+        self.opt = opt
+        self.pause = True
+
+    def process(self, images, return_time=False):
+        with torch.no_grad():
+            output = self.model(images)[-1]
+            hm = output['hm'].sigmoid_()
+            wh = output['wh']
+            reg = output['reg'] if self.opt.reg_offset else None
+            if self.opt.flip_test:
+                hm = (hm[0:1] + flip_tensor(hm[1:2])) / 2
+                wh = (wh[0:1] + flip_tensor(wh[1:2])) / 2
+                reg = reg[0:1] if reg is not None else None
+            torch.cuda.synchronize()
+            forward_time = time.time()
+            dets = ctdet_decode(hm, wh, reg=reg, cat_spec_wh=self.opt.cat_spec_wh, K=self.opt.K)
+
+        if return_time:
+            return output, dets, forward_time
+        else:
+            return output, dets
+
+    def post_process(self, dets, meta, scale=1):
+        dets = dets.detach().cpu().numpy()
+        dets = dets.reshape(1, -1, dets.shape[2])
+        dets = ctdet_post_process(
+            dets.copy(), [meta['c']], [meta['s']],
+            meta['out_height'], meta['out_width'], self.opt.num_classes)
+        for j in range(1, self.num_classes + 1):
+            dets[0][j] = np.array(dets[0][j], dtype=np.float32).reshape(-1, 5)
+            dets[0][j][:, :4] /= scale
+        return dets[0]
+
+    def merge_outputs(self, detections):
+        results = {}
+        for j in range(1, self.num_classes + 1):
+            results[j] = np.concatenate(
+                [detection[j] for detection in detections], axis=0).astype(np.float32)
+            if len(self.scales) > 1 or self.opt.nms:
+                soft_nms(results[j], Nt=0.5, method=2)
+        scores = np.hstack(
+            [results[j][:, 4] for j in range(1, self.num_classes + 1)])
+        if len(scores) > self.max_per_image:
+            kth = len(scores) - self.max_per_image
+            thresh = np.partition(scores, kth)[kth]
+            for j in range(1, self.num_classes + 1):
+                keep_inds = (results[j][:, 4] >= thresh)
+                results[j] = results[j][keep_inds]
+        return results
+
+    def debug(self, debugger, images, dets, output, scale=1):
+        detection = dets.detach().cpu().numpy().copy()
+        detection[:, :, :4] *= self.opt.down_ratio
+        for i in range(1):
+            img = images[i].detach().cpu().numpy().transpose(1, 2, 0)
+            img = ((img * self.std + self.mean) * 255).astype(np.uint8)
+            pred = debugger.gen_colormap(output['hm'][i].detach().cpu().numpy())
+            debugger.add_blend_img(img, pred, 'pred_hm_{:.1f}'.format(scale))
+            debugger.add_img(img, img_id='out_pred_{:.1f}'.format(scale))
+            for k in range(len(dets[i])):
+                if detection[i, k, 4] > self.opt.center_thresh:
+                    debugger.add_coco_bbox(detection[i, k, :4], detection[i, k, -1],
+                                           detection[i, k, 4],
+                                           img_id='out_pred_{:.1f}'.format(scale))
+
+    def show_results(self, debugger, original_image, results):
+        debugger.add_img(original_image, img_id='ctdet')
+        for j in range(1, self.num_classes + 1):
+            for bbox in results[j]:
+                if bbox[4] > self.opt.vis_thresh:
+                    bbox[:4] = self.dataset.pano.getOriginalCoord(original_image, bbox[:4])
+                    debugger.add_coco_bbox(bbox[:4], j - 1, bbox[4], img_id='ctdet')
+        debugger.show_all_imgs(pause=self.pause)
+
+    def pre_process(self, image, scale, meta=None):
+        height, width = image.shape[0:2]
+        new_height = int(height * scale)
+        new_width = int(width * scale)
+        if self.opt.fix_res:
+            inp_height, inp_width = self.opt.input_h, self.opt.input_w
+            c = np.array([new_width / 2., new_height / 2.], dtype=np.float32)
+            s = max(height, width) * 1.0
+        else:
+            inp_height = (new_height | self.opt.pad) + 1
+            inp_width = (new_width | self.opt.pad) + 1
+            c = np.array([new_width // 2, new_height // 2], dtype=np.float32)
+            s = np.array([inp_width, inp_height], dtype=np.float32)
+
+        trans_input = get_affine_transform(c, s, 0, [inp_width, inp_height])
+        resized_image = cv2.resize(image, (new_width, new_height))
+        inp_image = cv2.warpAffine(
+            resized_image, trans_input, (inp_width, inp_height),
+            flags=cv2.INTER_LINEAR)
+
+        images = inp_image.transpose(2, 0, 1).reshape(1, 3, inp_height, inp_width)
+        if self.opt.flip_test:
+            images = np.concatenate((images, images[:, :, :, ::-1]), axis=0)
+        images = torch.from_numpy(images)
+        meta = {'c': c, 's': s,
+                'out_height': inp_height // self.opt.down_ratio,
+                'out_width': inp_width // self.opt.down_ratio}
+        return images, meta
+
+    def run(self, dataset, img_id, meta=None):
+        load_time, pre_time, net_time, dec_time, post_time = 0, 0, 0, 0, 0
+        merge_time, tot_time = 0, 0
+        debugger = Debugger(dataset=self.opt.dataset, ipynb=(self.opt.debug == 3),
+                            theme=self.opt.debugger_theme)
+        start_time = time.time()
+
+        self.dataset = dataset
+        image, anns = self.dataset.pano.loadData(img_id)
+        original_image = cv2.imread(img_id)
+
+        loaded_time = time.time()
+        load_time += (loaded_time - start_time)
+
+        detections = []
+        for scale in self.scales:
+            scale_start_time = time.time()
+
+            images, meta = self.pre_process(image, scale, meta)
+
+            images = images.to(self.opt.device)
+            torch.cuda.synchronize()
+            pre_process_time = time.time()
+            pre_time += pre_process_time - scale_start_time
+
+            output, dets, forward_time = self.process(images, return_time=True)
+
+            torch.cuda.synchronize()
+            net_time += forward_time - pre_process_time
+            decode_time = time.time()
+            dec_time += decode_time - forward_time
+
+            if self.opt.debug >= 2:
+                self.debug(debugger, images, dets, output, scale)
+
+            dets = self.post_process(dets, meta, scale)
+            torch.cuda.synchronize()
+            post_process_time = time.time()
+            post_time += post_process_time - decode_time
+
+            detections.append(dets)
+
+        results = self.merge_outputs(detections)
+        torch.cuda.synchronize()
+        end_time = time.time()
+        merge_time += end_time - post_process_time
+        tot_time += end_time - start_time
+
+        if self.opt.debug >= 1:
+            self.show_results(debugger, original_image, results)
+
+        return {'results': results, 'tot': tot_time, 'load': load_time,
+                'pre': pre_time, 'net': net_time, 'dec': dec_time,
+                'post': post_time, 'merge': merge_time}
